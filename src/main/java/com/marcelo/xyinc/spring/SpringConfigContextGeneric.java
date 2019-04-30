a21fd918b8a08784fe64fa10c223fb2a5ab8ee0d
DinosNao <preis.udesc@gmail.com>
2019-04-29 21:52:28 -0300

    Configuration for Spring access database

--- src/main/java/com/marcelo/xyinc/spring/SpringConfigContextGeneric.java
+++ src/main/java/com/marcelo/xyinc/spring/SpringConfigContextGeneric.java
@@ -25,91 +25,101 @@ import org.springframework.transaction.support.TransactionTemplate;
 @EnableTransactionManagement
 @ComponentScan(basePackages = {
 
-		// mapping repository
-		"com.marcelo.xyinc.spring.repository",
+	// mapping repository
+	"com.marcelo.xyinc.spring.repository",
 
-		// mapping services
-		"com.marcelo.xyinc.spring.service",
+	// mapping services
+	"com.marcelo.xyinc.spring.service",
 
-		// maping the resouces extra project
-		"com.marcelo.xyinc.filters" })
+	// maping the resouces extra project
+	"com.marcelo.xyinc.filters" })
 public class SpringConfigContextGeneric extends AnnotationConfigApplicationContext implements BeanFactory {
 
-	private static final Log LOG = LogFactory.getLog(SpringConfigContextGeneric.class);
+    private static final Log LOG = LogFactory.getLog(SpringConfigContextGeneric.class);
 
-	/**
-	 * DataSource to access the system database
-	 *
-	 * @return: DataSource
-	 */
-	@Bean(name = "dataSource")
-	@Autowired
-	public DataSource restDataSource() {
-		return DataSourceBuilder.create().username("").password("").url("").driverClassName("").build();
-	}
+    /**
+     * DataSource to access the system database
+     *
+     * @return: DataSource
+     */
+    @Bean(name = "dataSource")
+    @Autowired
+    public DataSource restDataSource() {
+	Properties properties = loadPropertiesForHibernate();
+	DataSourceBuilder<?> dataSouce = DataSourceBuilder.create();
+	dataSouce.username(properties.getProperty("hibernate.connection.username"));
+	dataSouce.password(properties.getProperty("hibernate.connection.password"));
+	dataSouce.url(properties.getProperty("hibernate.connection.url"));
+	dataSouce.driverClassName(properties.getProperty("hibernate.connection.driver_class"));
+	return dataSouce.build();
+    }
 
-	/**
-	 * SessionFactory of the Hibernate,to build the access factory to the bank by
-	 * the hibernate, a hibernateSessionFactory
-	 *
-	 * @param: DataSource.
-	 * @return: SessionFactory is singleton
-	 */
-	@Bean(name = "sessionFactory")
-	@Autowired
-	public SessionFactory sessionFactory(@Qualifier(value = "dataSource") final DataSource dataSource)
-			throws IOException {
-		final LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-		sessionFactoryBean.setDataSource(dataSource);
-		sessionFactoryBean.setHibernateProperties(loadPropertiesForHibernate());
-		final Class<?>[] annotatedClasses = new Class<?>[] {/*add our class here */ };
-		sessionFactoryBean.setAnnotatedClasses(annotatedClasses);
-		sessionFactoryBean.afterPropertiesSet();
-		LOG.info("configured SessionFactory bean: sessionFactory");
-		return sessionFactoryBean.getObject();
-	}
+    /**
+     * SessionFactory of the Hibernate,to build the access factory to the bank by
+     * the hibernate, a hibernateSessionFactory
+     *
+     * @param: DataSource.
+     * @return: SessionFactory is singleton
+     */
+    @Bean(name = "sessionFactory")
+    @Autowired
+    public SessionFactory sessionFactory(@Qualifier(value = "dataSource") final DataSource dataSource)
+	    throws IOException {
+	final LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+	sessionFactoryBean.setDataSource(dataSource);
+	sessionFactoryBean.setHibernateProperties(loadPropertiesForHibernate());
+	final Class<?>[] annotatedClasses = new Class<?>[] { /* add our class here */ };
+	sessionFactoryBean.setAnnotatedClasses(annotatedClasses);
+	sessionFactoryBean.afterPropertiesSet();
+	LOG.info("configured SessionFactory bean: sessionFactory");
+	return sessionFactoryBean.getObject();
+    }
 
-	private Properties loadPropertiesForHibernate() {
-		final Properties properties = new Properties();
-		properties.put("hibernate.connection.driver_class", null);
-		properties.put("hibernate.connection.url", null);
-		properties.put("hibernate.connection.username", null);
-		properties.put("hibernate.connection.password", null);
-		return properties;
-	}
+    /** define date access database */
+    private Properties loadPropertiesForHibernate() {
+	final Properties properties = new Properties();
+	properties.put("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
+	properties.put("hibernate.connection.url", "jdbc:hsqldb:mem:testdb");
+	properties.put("hibernate.connection.username", "sa");
+	properties.put("hibernate.connection.password", "");
+	// only for session factory
+	properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+	properties.put("hibernate.hbm2ddl.auto", "update");
+	return properties;
+    }
 
-	/**
-	 * To manage hibernate transactions
-	 *
-	 * @param SessionFactory
-	 * @return HibernateTransactionManager
-	 */
-	@Bean(name = "transactionManager")
-	@Autowired
-	public HibernateTransactionManager transactionManager(
-			@Qualifier(value = "sessionFactory") final SessionFactory sessionFactory) {
-		LOG.info("start HibernateTransactionManager");
-		final HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-		transactionManager.afterPropertiesSet();
-		LOG.info("finish HibernateTransactionManager");
-		return transactionManager;
-	}
+    /**
+     * To manage hibernate transactions
+     *
+     * @param SessionFactory
+     * @return HibernateTransactionManager
+     */
+    @Bean(name = "transactionManager")
+    @Autowired
+    public HibernateTransactionManager transactionManager(
+	    @Qualifier(value = "sessionFactory") final SessionFactory sessionFactory) {
+	LOG.info("start HibernateTransactionManager");
+	final HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+	transactionManager.afterPropertiesSet();
+	LOG.info("finish HibernateTransactionManager");
+	return transactionManager;
+    }
 
-	/**
-	 * To manage hibernate TransactionTemplate
-	 *
-	 * @param HibernateTransactionManager
-	 * @return TransactionTemplate
-	 */
-	@Bean(name = "transactionTemplate")
-	@Autowired
-	public TransactionTemplate transactionTemplate(
-			@Qualifier(value = "transactionManager") final HibernateTransactionManager transactionManager) {
-		LOG.info("start HibernateTransactionManager");
-		final TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-		transactionManager.afterPropertiesSet();
-		LOG.info("finish HibernateTransactionManager");
-		return transactionTemplate;
-	}
+    /**
+     * To manage hibernate TransactionTemplate
+     *
+     * @param HibernateTransactionManager
+     * @return TransactionTemplate
+     */
+    @Bean(name = "transactionTemplate")
+    @Autowired
+    public TransactionTemplate transactionTemplate(
+	    @Qualifier(value = "transactionManager") final HibernateTransactionManager transactionManager) {
+	LOG.info("start HibernateTransactionManager");
+	final TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+	transactionManager.afterPropertiesSet();
+	LOG.info("finish HibernateTransactionManager");
+	return transactionTemplate;
+    }
 
 }
