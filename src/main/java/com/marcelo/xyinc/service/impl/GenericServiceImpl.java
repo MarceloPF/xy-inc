08df6849f1af1a5a910fbfe6a2e318e8acf7a296
DinosNao <preis.udesc@gmail.com>
2019-05-01 21:40:57 -0300

    Create a generic class, for layer service for system

---
+++ src/main/java/com/marcelo/xyinc/service/impl/GenericServiceImpl.java
@@ -0,0 +1,77 @@
+package com.marcelo.xyinc.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.marcelo.xyinc.repository.GenericRepository;
+import com.marcelo.xyinc.service.GenericService;
+
+@Service(value = "genericServiceImpl")
+@Transactional
+public class GenericServiceImpl<E extends Serializable, K extends Serializable> implements GenericService<E, K> {
+
+    private GenericRepository<E, K> genericRepository;
+
+    public GenericServiceImpl(final GenericRepository<E, K> genericRepository) {
+	this.genericRepository = genericRepository;
+    }
+
+    @Override
+    @Autowired
+    public void setDAO(final @Qualifier("genericRepositoryImpl") GenericRepository<E, K> genericRepository) {
+	this.genericRepository = genericRepository;
+    }
+
+    @Override
+    public GenericRepository<E, K> getDAO() {
+	return genericRepository;
+    }
+
+    @Override
+    public E save(E object) {
+	final E result;
+	if (validPreSave(object)) {
+	    result = genericRepository.save(object);
+	} else {
+	    result = null;
+	}
+	return result;
+    }
+
+    @Override
+    public E update(E object) {
+	final E result;
+	if (validPreSave(object)) {
+	    result = genericRepository.update(object);
+	} else {
+	    result = null;
+	}
+	return result;
+    }
+
+    @Override
+    public E findById(K key, Class<?> clazz) {
+	return genericRepository.findById(key, clazz);
+    }
+
+    @Override
+    public List<E> findAll(Class<?> clazz) {
+	return genericRepository.findAll(clazz);
+    }
+
+    @Override
+    public void delete(E object) {
+	genericRepository.delete(object);
+    }
+
+    @Override
+    public boolean validPreSave(E object) {
+	return true;
+    }
+
+}
