72861e516bfc17e4e28a2ef8be2f621c9805614d
DinosNao <preis.udesc@gmail.com>
2019-05-04 23:16:21 -0300

    Configure rotas for requests

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PointListComponent } from './point-list/point-list.component';
+import { CreatePointComponent } from './create-point/create-point.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'point', pathMatch: 'full' },
+    { path: 'points', component: PointListComponent },
+    { path: 'add', component: CreatePointComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
