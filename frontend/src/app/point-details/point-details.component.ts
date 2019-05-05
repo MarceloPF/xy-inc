dcf3c919b282e6869a4a17f08fa8153d635a076d
DinosNao <preis.udesc@gmail.com>
2019-05-04 23:04:53 -0300

    Programmed to show the details of the point

--- frontend/src/app/point-details/point-details.component.ts
+++ frontend/src/app/point-details/point-details.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Point } from '../point';
+import { PointService } from '../point.service';
+import { PointListComponent } from '../point-list/point-list.component';
 
 @Component({
-  selector: 'app-point-details',
-  templateUrl: './point-details.component.html',
-  styleUrls: ['./point-details.component.css']
+    selector: 'app-point-details',
+    templateUrl: './point-details.component.html',
+    styleUrls: ['./point-details.component.css']
 })
 export class PointDetailsComponent implements OnInit {
 
-  constructor() { }
+    @Input() point: Point;
 
-  ngOnInit() {
-  }
+
+    constructor(private pointService: PointService, private listComponent: PointListComponent) { }
+
+    ngOnInit() {
+    }
 
 }
