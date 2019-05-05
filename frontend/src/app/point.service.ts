5b5b7f5132100ecd6935a517d870694943f616b5
DinosNao <preis.udesc@gmail.com>
2019-05-04 22:26:15 -0300

    Customize a service for call RestAPI-Service

--- frontend/src/app/point.service.ts
+++ frontend/src/app/point.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PointService {
 
-  constructor() { }
+    private baseUrl = '/rest/api/v1/point';
+
+  constructor(private http: HttpClient) { }
+
+  createPoint(point: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, point);
+  }
+
+  updatePoint(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deletePoint(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  getPointsList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
 }
