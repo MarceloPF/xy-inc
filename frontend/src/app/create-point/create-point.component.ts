1e83c175027bfd5f8b7d6ff10871a4fb3e63adc4
DinosNao <preis.udesc@gmail.com>
2019-05-04 22:54:26 -0300

    Add new funcionality save, new, send

--- frontend/src/app/create-point/create-point.component.ts
+++ frontend/src/app/create-point/create-point.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Point } from '../point';
+import { PointService } from '../point.service';
 
 @Component({
-  selector: 'app-create-point',
-  templateUrl: './create-point.component.html',
-  styleUrls: ['./create-point.component.css']
+    selector: 'app-create-point',
+    templateUrl: './create-point.component.html',
+    styleUrls: ['./create-point.component.css']
 })
 export class CreatePointComponent implements OnInit {
 
-  constructor() { }
+    point: Point = new Point();
+    submitted = false;
 
-  ngOnInit() {
-  }
+    constructor(private pointService: PointService) {
+    }
+
+    ngOnInit() {
+    }
+
+    newPoint(): void {
+        this.submitted = false;
+        this.point = new Point();
+    }
+
+    save() {
+        this.pointService.createPoint(this.point)
+            .subscribe(data =>
+                console.log(data),
+                error => console.log(error));
+        this.point = new Point();
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        this.save();
+    }
 
 }
