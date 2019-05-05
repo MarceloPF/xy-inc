a69c37ef47771ae5f1743628e3fd13be41f6de5e
DinosNao <preis.udesc@gmail.com>
2019-05-04 22:38:50 -0300

    Configure PointListComponent for access data, and can delete objects

--- frontend/src/app/point-list/point-list.component.ts
+++ frontend/src/app/point-list/point-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Point } from '../point';
+import { Observable } from 'rxjs';
+import { PointService } from '../point.service';
 
 @Component({
-  selector: 'app-point-list',
-  templateUrl: './point-list.component.html',
-  styleUrls: ['./point-list.component.css']
+    selector: 'app-point-list',
+    templateUrl: './point-list.component.html',
+    styleUrls: ['./point-list.component.css']
 })
 export class PointListComponent implements OnInit {
 
-  constructor() { }
+    points: Observable<Point[]>;
 
-  ngOnInit() {
-  }
+    constructor(private pointService: PointService) { }
+
+    ngOnInit() {
+        this.reloadData();
+    }
+
+    reloadData() {
+        this.points = this.pointService.getPointsList();
+    }
+
+    deletePoit(id: number) {
+        this.pointService.deletePoint(id)
+            .subscribe(
+                data => {
+                    console.log(data);
+                    this.reloadData();
+                },
+                error => console.log(error));
+    }
 
 }
